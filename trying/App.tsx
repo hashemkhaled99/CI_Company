d1c35a6c7d29d103391823028859e70e654b3133
hashemkhaled99 <hashem@sijilacc.com>
2026-04-27 11:36:01 +0300

    react test

---
+++ trying/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function App({ title }: { title: string }) {
+	if (title === "Error") {
+		return <div>Error State</div>;
+	}
+	return (
+		<div>
+			<h1>{title}</h1>
+		</div>
+	);
+}
